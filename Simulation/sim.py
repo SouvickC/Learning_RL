886df94d4362b56b089d8855c5aeecec006a8ba2
SouvickC <souvickchakraborty100@gmail.com>
2026-01-10 19:43:39 +0100

    test mujoco simulation for spot robot and video record

--- Simulation/sim.py
+++ Simulation/sim.py
@@ -3,6 +3,8 @@ import mujoco.viewer
 import numpy as np
 import time
 import mujoco_viewer
+import PIL.Image
+import imageio
 
 
 MODEL_PATH = r"D:\CS\RL\mujoco_menagerie\boston_dynamics_spot\scene.xml"
@@ -11,7 +13,7 @@ data = mujoco.MjData(model)
 
 
 # # create the viewer object
-viewer = mujoco_viewer.MujocoViewer(model, data, 'offscreen')
+viewer = mujoco_viewer.MujocoViewer(model, data)
 
 viewer.add_line_to_fig(line_name="root-pos-x", fig_idx=0)
 viewer.add_line_to_fig(line_name="root-pos-z", fig_idx=0)
@@ -34,7 +36,6 @@ fig.figurergba[0] = 0.2
 fig.figurergba[3] = 0.2
 
 
-import numpy as np
 
 def get_trot_gait(t, period=1.0, hip_range=0.6, knee_range=0.4, hip_offset=0.8, knee_offset=-1.5):
     """
@@ -70,41 +71,71 @@ def get_trot_gait(t, period=1.0, hip_range=0.6, knee_range=0.4, hip_offset=0.8, 
         hx_vals[3], hr_hy, hr_kn
     ])
 
+# visualization 
+
 start_time = time.time()
 while viewer.is_alive:
+
+
     viewer.add_data_to_line(line_name="root-pos-x",
                             line_data=data.qpos[0], fig_idx=0)
     viewer.add_data_to_line(line_name="root-pos-z",
                             line_data=data.qpos[2], fig_idx=0)
     viewer.add_data_to_line(line_name="right_ankle_y", line_data=data.qpos[
         mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, "right_ankle_y")], fig_idx=1)
+
+
     # sim step
     t = time.time() - start_time
     data.ctrl[:] = get_trot_gait(t, period=0.8)
+
     mujoco.mj_step(model, data)
 
     time.sleep(0.01)
     # render
-    viewer.render()
+    viewer.render()   
+
+
     if not viewer.is_alive:
         break
 
-
-
-# # simulate and render
-# for _ in range(100000):
-#     viewer.add_data_to_line(line_name="root-pos-x",
-#                             line_data=data.qpos[0], fig_idx=0)
-#     viewer.add_data_to_line(line_name="root-pos-z",
-#                             line_data=data.qpos[2], fig_idx=0)
-#     viewer.add_data_to_line(line_name="right_ankle_y", line_data=data.qpos[
-#         mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, "right_ankle_y")], fig_idx=1)
-
-#     mujoco.mj_step(model, data)
-#     viewer.render()
-#     if not viewer.is_alive:
-#         break
-
-
-# close
 viewer.close()
+
+
+# import time
+# import mujoco
+# import imageio
+# import numpy as np
+
+# start_time = time.time()
+# framerate = 60
+# dt = 1.0 / framerate
+# frames = []
+
+# with mujoco.Renderer(model, 480, 640) as renderer:
+#     t = 0.0
+#     while t <= 60.0:
+#         # control
+#         data.ctrl[:] = get_trot_gait(t, period=0.8)
+
+#         # step physics with fixed dt (optional but nice for video)
+#         mujoco.mj_step(model, data)
+
+#         # render offscreen
+#         renderer.update_scene(data)
+#         pixels = renderer.render()          # H x W x 3 float32 in [0, 1]
+#         frame = (pixels).astype(np.uint8)
+#         frames.append(frame)
+
+#         t = time.time() - start_time
+
+# print(len(frames))
+
+# # save as mp4
+# imageio.mimsave(
+#     "spot.mp4",
+#     frames,
+#     fps=framerate,
+#     codec="libx264",
+#     macro_block_size=None,   # important if 480x640 not multiple of 16
+# )
